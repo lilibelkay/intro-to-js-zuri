5ba5ab79fb863856c473136feb103c90b210e7ac
lilibelkay <lilibelkosiso@gmail.com>
2022-06-21 23:28:25 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let name = "Obiadika Lilibel Kosisochukwu"
+let height = "161cm";
+let country = "Nigeria"
+
+document.write("My name is " + name)
+document.write("<br>");
+document.write("I am " + height + " tall.");
+document.write("<br>");
+document.write("I am from " + country + ".");
